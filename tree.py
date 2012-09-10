82846314120bdbbe671863e4bb70e410a8db244d
Yirba <leonbyford@Leons-MacBook-Pro.local>
2012-09-10 16:14:20 +0100

    documentation for tree.py

--- tree.py
+++ tree.py
@@ -1,3 +1,16 @@
+############################################
+# tree.py                                  #
+# Developed by Yirba <http://yirba.x10.mx> #
+# Created: 10 September 2012               #
+# Last modified: 10 September 2012         #
+############################################
+# Description:                             #
+#   Prints a tree out of asterisks in the  #
+# specified size. For best results,        #
+# treeWidth should be an odd multiple of   #
+# 3.                                       #
+############################################
+
 def printCrown(treeWidth):
     for i in range(1,treeWidth+1,2):
         print(("*"*i).center(treeWidth))
